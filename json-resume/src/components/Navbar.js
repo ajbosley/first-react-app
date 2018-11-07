b7f34316aefd419e9134b2de9dbf382138ad8fa7
Unknown <aaron@ajbosley.co.uk>
2018-11-07 22:41:20 +0000

    Navbar structural clean-up and Resume dummy text

--- json-resume/src/components/Navbar.js
+++ json-resume/src/components/Navbar.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
+import Navitems from './Navitems';
 import '../styles/Navbar.css';
 
 class Navbar extends Component {
     render(){
         return(
-            <nav>
-                <div className="nav-wrapper">
-                    <a>Logo</a>
-                    <ul id="mobile-nav" className="right">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Resume</a></li>
-                        <li><a href="#"><i className="material-icons">menu</i></a></li>
-                    </ul>
+                <div className="navBar">
+                    <Navitems />
                 </div>
-            </nav>
         )
     }   
 }
